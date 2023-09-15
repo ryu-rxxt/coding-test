71eee63023b8d0d0c0e3673787bbb840d621cebb
ryu-rxxt <140266166+ryu-rxxt@users.noreply.github.com>
2023-09-15 18:22:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 288 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,20 @@
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	fastio();
+	int N;
+	cin >> N;
+	vector<int> arr(N);
+	
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+	
+	sort(arr.begin(), arr.end());
+	
+	for (int i = 0; i < N; i++)
+		cout << arr[i] << '\n';
+}
