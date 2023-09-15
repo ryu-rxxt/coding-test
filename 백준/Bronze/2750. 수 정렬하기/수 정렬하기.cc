8b3d7a3103e01fc068574fc5d6795ffea4e9babc
ryu-rxxt <140266166+ryu-rxxt@users.noreply.github.com>
2023-09-15 18:21:36 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,6 +1,7 @@
 #define fastio() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
@@ -12,18 +13,7 @@ int main() {
 	for (int i = 0; i < N; i++)
 		cin >> arr[i];
 	
-	for (int i = 0; i < N - 1; i++)
-	{
-		for (int j = 0; j < N - 1 - i; j++)
-		{
-			if (arr[j] > arr[j + 1])
-				{
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-		}
-	}
+	sort(arr.begin(), arr.end());
 	
 	for (int i = 0; i < N; i++)
 		cout << arr[i] << '\n';
