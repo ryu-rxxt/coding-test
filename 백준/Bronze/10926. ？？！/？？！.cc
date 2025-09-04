221f9c46d735871e2a249ac6d447d59fea24dedb
ryu-rxxt <140266166+ryu-rxxt@users.noreply.github.com>
2025-09-04 11:34:58 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,11 @@
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    fastio();
+    string ID;
+    cin >> ID;
+    cout << ID + "??!";
+}
