966b07f3528419bc3ee9d8f68cee42abbcec02b2
ryu-rxxt <140266166+ryu-rxxt@users.noreply.github.com>
2025-09-04 11:37:18 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,10 @@
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#include <iostream>
+using namespace std;
+
+int main() {
+    fastio();
+    int y;
+    cin >> y;
+    cout << y - 543;
+}
