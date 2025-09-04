cff375bf212c6d9528d4a52917a3934e4f2dd205
ryu-rxxt <140266166+ryu-rxxt@users.noreply.github.com>
2025-09-04 13:41:30 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,10 @@
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#include <iostream>
+using namespace std;
+
+int main() {
+    fastio();
+    long long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C;
+}
