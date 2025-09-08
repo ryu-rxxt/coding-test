efb8ef90c0a80c38c9e9be73aaa170f8d3905641
ryu-rxxt <140266166+ryu-rxxt@users.noreply.github.com>
2025-09-08 10:47:04 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,19 @@
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    fastio();
+    int N;
+    cin >> N;
+    vector<int> dp(N + 1);
+    dp[1] = 0;
+    for (int i = 2; i < N + 1; i++) {
+        dp[i] = dp[i - 1] + 1;
+        if (!(i % 3)) dp[i] = min(dp[i], dp[i / 3] + 1);
+        if (!(i % 2)) dp[i] = min(dp[i], dp[i / 2] + 1);
+    }
+    cout << dp[N];
+}
